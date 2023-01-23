16dde5d961640a3cbe61bc56710adc2f9bfacf56
JackHyde <jack.hyde@school.barren.kyschools.us>
2023-01-23 10:53:16 -0600

    completed

---
+++ script.js
@@ -0,0 +1,20 @@
+//Literal Math Method Exercise
+let warmHugs = "Hi, I'm Olaf and I like warm hugs.";
+console.log(warmHugs.toUpperCase());
+warmHugs = warmHugs.replace("like", "love");
+var beenImpaled = "Oh, look at that. I've been impaled.";
+console.log(beenImpaled.slice(18));
+var dotDotDot = "...";
+var skullBones = `I don't have a skull${dotDotDot}or bones.`;
+console.log(Math.PI);
+var randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log(randomNumber);
+
+// BONUS
+console.log(" Let It Go!".toUpperCase().repeat(2).trim());
+var reindeers = "Reindeers are better than people.";
+reindeers = reindeers.replace(" ", "_").replace(" ", "_").replace(" ", "_").replace(" ", "_");
+console.log(reindeers);
+console.log(Math.sqrt(2));
+var randomNumber = Math.floor((Math.random() * 16 + 8));
+console.log(randomNumber);
